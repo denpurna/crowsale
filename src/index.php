21a0c583dcd69bb6b1ca9634b9c5ab1ea8b3ba8d
rekayasa79 <denipurnama371@gmail.com>
2019-11-26 19:41:15 +0700

    ara

---
+++ src/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CROW ICO Sale</title>
+    <!-- Bootstrap -->
+   	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/themify-icons.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+  </head>
+  <body class="text-center">
+      
+<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+      
+      <div class="clearfix"></div>
+				<h2>CROW SALE</h2>
+		
+            <hr/>
+      	<section id="content" style="display:;" class="features-section spad gradient-bg">
+		<div class="container text-white">
+	<div class="section-title text-center">
+           <h5> Introducing "CROW" (CC)!</h5>
+            Token price is <span class="token-price">0.0001</span> Ether. You currently have <span class="dapp-balance">0</span>&nbsp;CC.
+            </div>
+          			<div class="row">
+				<!-- feature -->
+				<div class="col-md-6 col-lg-4 feature">
+          <form class="hero-subscribe-from" onSubmit="App.buyTokens(); return false;" role="form">
+                <input id="numberOfTokens" type="number" name="number" value="1000" min="1000" pattern="[0-9]">
+                </br>
+               
+                  <button type="submit" class="site-btn sb-gradients">Buy Tokens</button>
+                
+          </form>
+          <br>
+           <div class="progress">
+              <div id="progress" class="progress-bar progress-bar-striped active" aria-valuemin="0" style="width: 10%;" aria-valuenow="10" aria-valuemax="100">10%
+              </div>
+            </div>
+          <p><span class="tokens-sold">750000</span> / <span class="tokens-available">7500000</span> Tokens Sold</p>
+<sub class="text-warning">Note: Minimum 1000 CC</sub>
+          <hr>
+          <p id="accountAddress"><font style="color:yellow">Please connect your wallet (recomended: metamask)</font></p>
+        </div>
+        </div>
+        </div>
+        </section>
+      
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/web3.min.js"></script>
+    <script src="js/truffle-contract.min.js"></script>
+    <script src="js/app.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/main.js"></script>
+  </body>
+</html>
