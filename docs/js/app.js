bd0d9b5ab4fd422ee96e564d2fd377cb5009e5ee
rekayasa79 <denipurnama371@gmail.com>
2019-11-30 17:39:12 +0700

    ah teuing

--- docs/js/app.js
+++ docs/js/app.js
@@ -27,14 +27,16 @@ App = {
  var abiToken = $.getJSON('https://denpurna.github.io/crowsale/tokenAbi.json');
 var MyContract = web3.eth.contract(abiToken);
 var myContractInstance = MyContract.at('0x4093Db3B3c52cb24A2C239820bc7960575af0401');
-    var filter = myContractInstance.balanceOf({0x9b11f66A0868392eDF372A32760Ce0dEFE6eC20b}, function (error, result) {
+    var filter = myContractInstance.balanceOf('0x9b11f66A0868392eDF372A32760Ce0dEFE6eC20b', function (error, result) {
  if (!error){
  	alert(result);
  	 $('#dapp-balance').append(result.toString());
       }
+      else{
+      	alert(error)
       }
-      );
-                       }
+      });
+         }
         });
 });
     } catch(e) {
